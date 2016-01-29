e776c04651cb153d13a2bc07c770562d49ed60f8
MartiniMoe <martini.moe@cheex.de>
2016-01-30 00:14:32 +0100

    fix lava stuff

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,25 @@
 extends KinematicBody2D
 
+var tilemap
+
 func _ready():
+	tilemap = get_parent().get_node("level01")
 	set_fixed_process(true)
 
 func _fixed_process(delta):
+	move_player()
+	check_collisions()
+
+func check_collisions():
+	var tile_x = floor(get_pos().x / tilemap.get_cell_size().x)
+	var tile_y = floor(get_pos().y / tilemap.get_cell_size().y)
+	var tile_pos = tilemap.world_to_map(self.get_pos())
+	var tile_index = tilemap.get_cell(tile_pos.x, tile_pos.y)
+	#print("im on tile #" + str(tile_index) + " " + str(tile_pos.x) + "|" + str(tile_pos.y))
+	if tile_index == 1:
+		print("lava!!!!")
+
+func move_player():
 	var move_force = 2
 	var move_x = 0
 	var move_y = 0
@@ -16,4 +32,4 @@ func _fixed_process(delta):
 	if Input.is_action_pressed("player_01_left"):
 		move_x -= move_force
 	
-	move(Vector2(move_x, move_y))
+	self.move(Vector2(move_x, move_y))
