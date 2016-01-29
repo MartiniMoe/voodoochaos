b5680e67e611721107d5fad1aed34567fa99ee4a
christianjung87 <git@chris-jung.de>
2016-01-30 00:41:21 +0100

    fixed main.gd

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,12 +4,11 @@ func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
-	var cam = get_node("Viewport/main_camera")
+	var cam = get_node("main_camera")
 	var vp = get_node("Viewport")
 	
 	var cam_speed=0.5
 	var cam_x = cam.get_offset().x+cam_speed
-	
 	
 	cam.make_current()	
 	cam.set_offset(Vector2(cam_x, 0))
