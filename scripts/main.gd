5ac7786934519fa446349c0f9a6496ea2eba424b
MartiniMoe <martini.moe@cheex.de>
2016-01-30 20:40:35 +0100

    add doll to main

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,17 +1,19 @@
 extends Node2D
 
-func _ready():	
+var cam
+var vp
+var doll
+var cam_speed=0.5
+
+func _ready():
+	cam = get_node("main_camera")
+	vp = get_node("Viewport")
+	doll = get_node("doll")
 	set_fixed_process(true)
 
 func _fixed_process(delta):
-	#get_node("doll_render").set_texture(get_node("doll").get_render_target_texture())
-	#get_node("doll/Viewport/Camera2D").make_current()
-	
-	var cam = get_node("main_camera")
-	var vp = get_node("Viewport")
-	
-	var cam_speed=0.5
 	var cam_x = cam.get_offset().x+cam_speed
 	
-	cam.make_current()	
+	cam.make_current()
 	cam.set_offset(Vector2(cam_x, 0))
+	doll.set_pos(Vector2(cam.get_offset().x - (get_viewport().get_rect().end.x/2) + doll.get_texture().get_width(), doll.get_pos().y))
